485aa3883ee269c7d2f378cf01d64ae27b020c67
Farhan <95966734+FarhanKhalawi@users.noreply.github.com>
2025-08-20 12:41:10 +0200

    Add files via upload

---
+++ Back-end/djangoProject/djangoProject/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('position/<lat>/<lon>/', views.main_api, name='main_api'),
+]
